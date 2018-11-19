f75bf2dd8b225a8d221d5b7e856389adf24bf8d6
Baibhav Vatsa <baibhavvatsa@gmail.com>
2018-11-19 01:09:29 -0600

    outline for the code
    the basic outline for the code done

---
+++ background.js
@@ -0,0 +1,34 @@
+String url = window.location.href;
+const String correctPage = "https://acad.app.vanderbilt.edu/more/SearchClasses"; 
+const String query = "http://www.ratemyprofessors.com/search.jsp?query=";
+
+if (url.includes(correctPage)) {
+	
+	String profFullName = document.getElementbyClass("classInstructor").innerHTML.trim();
+	String profFirstName = profFullName.splice(0, profFullName.indexOf(','));
+//	String profLastName = profFullName.splice(profFullName.indexOf(','),);		yet to be implemented completely
+	String searchString = query + profFirstName;
+	window.open(searchString, "_newtab");
+	int found = 0;
+//	loop through the webpages till you run out of pages
+//	or the name is found. "listings", "listing PROFESSOR", "listing-name", "sub"
+//	if they have both in common, check the first name also
+	if(found==1) {
+//		a href = professor's ratings link
+//		"grade" for rating
+//		int rating = parseInt();			yet to be implemented completely
+		if(rating > 3.5) {
+//			show in green the professor's name and rating
+		}
+		else if (rating > 2.0) {
+//			show in orange the professor's name and rating
+		}
+		else {
+//			show in red the professor's name and rating
+		}
+	}
+
+}
+else {
+	document.innerHTML = "Hello Baibhav!"
+}
