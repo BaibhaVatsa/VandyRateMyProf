402be655c3d17896dad3555147fd76d8eb406562
ahmednafis00 <ahmednafis00@gmail.com>
2018-11-20 18:02:31 -0600

    added pseudocode

--- background.js
+++ background.js
@@ -1,20 +1,27 @@
-//the string which will the professor's name and "vanderbilt" added to it
+//the string to which the professor's last name and "vanderbilt" will be added to
 const String query = "http://www.ratemyprofessors.com/search.jsp?query=";
-while() {
-	while() {
+while() { //tables in search results page in YES (Student Portal)
+	while() { //there are rows of results 
+		//pick classInstructor
 		String profFullName = document.getElementbyClass("classInstructor").innerHTML.trim();
-		String profFirstName = profFullName.splice(0, profFullName.indexOf(','));
-	//	String profLastName = profFullName.splice(profFullName.indexOf(','),);		yet to be implemented completely
-		String searchString = query + profFirstName + "vanderbilt";
-	//	window.open(searchString, "_newtab");
+		String profLastName = profFullName.splice(0, profFullName.indexOf(','));
+	//	String profFirstName = profFullName.splice(profFullName.indexOf(','),);		yet to be implemented completely
+		String searchString = query + profLastName + " vanderbilt";
+	//	window.open(searchString, "_newtab"); 	FIND ALTERNATIVE AS THIS DOESN"T LET THE EXTENSION WORK IN BACKGROUND
+		
 		int found = 0;
-	//	loop through the webpages till you run out of pages
-	//	or the name is found. "listings", "listing PROFESSOR", "listing-name", "sub"
-	//	if they have both in common, check the first name also
-		if(found==1) {
+	//	while() there are list items in listings
+			//access main of listing prof
+			//if main has profLastName, profFirstName
+				//go to link in href
+				//access grade i.e. rating
+
+	//	change inner HTML to profFullName + " ~ " + rating 
+
+			if(found==1) {
 	//		a href = professor's ratings link
 	//		"grade" for rating
-	//		int rating = parseInt();			yet to be implemented completely
+	//		int rating = parseInt();			yet to be `implemented completely
 			if(rating > 3.5) {
 		//			show in green the professor's name and rating
 			}
